347762a8010ee3cf35549f4450c9da79d9acd03d
Sunny Vempati <sunny.vempati@centro.net>
2014-04-30 18:19:47 -0500

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+#user
+u = User.first
+u ||= User.create!(email:"sunny@yourplannr.com", 
+						password: "nutella", password_confirmation: "nutella", first_name: "sunny", last_name: "vempati")
+
+if u.events.count == 0
+	10.times.each do |e|
+		event_params = 
+		{
+			title: 				Faker::Lorem.sentence(3),
+			date: 				Faker::Number.number(2).to_i.weeks.from_now,
+			guest_count: 	Faker::Number.number(2),
+			min_budget: 	Faker::Number.number(3),
+			max_budget: 	Faker::Number.number(4),
+			location:			Faker::Address.city,
+	  	details:			Faker::Lorem.paragraph,
+	  	planner:			Faker::Name.name,
+	  	status:  			Faker::Lorem.word,
+		}
+
+		e = Event.new(event_params)
+		u.events.create(event_params) if e.valid?
+	end
+end
