c1c85a49ddcc7bf4310b80f2054cf091d9b8a1ba
nguyenj <johnnguyen.jln@gmail.com>
2014-05-01 09:59:58 -0500

    update homepage. add body id/class methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
+
+  def set_body_id(string)
+    @body_id = string
+  end
+
+  def set_body_class(array)
+    @body_class ||= []
+    @body_class = @body_class + Array(array)
+  end
+
 end
