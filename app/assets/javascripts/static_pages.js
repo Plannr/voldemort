8f517d821b05b537a04ad5f3864b5823c5019bae
nguyenj <johnnguyen.jln@gmail.com>
2014-05-01 09:00:41 -0500

    add hero section

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var app = app || {};
+
+app.init = function () {
+    app.bgFull(".hero.bg-full");
+};
+
+app.bgFull = function ( selector ) {
+    var el = $(selector);
+    var imgSrc = el.data('image-src');
+
+    console.log(el.css('background-image', 'url('+imgSrc+')'))
+};
+
+$(document).on('ready page:load', function () {
+    app.init();
+});
