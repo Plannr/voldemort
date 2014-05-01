394347a3b0dcf42ca3f6946d89f9d23c4f82838e
nguyenj <johnnguyen.jln@gmail.com>
2014-05-01 09:59:58 -0500

    update homepage. add body id/class methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
+
+  before_filter { set_body_id "pages" }
+
   def index
+    set_body_class "homepage"
+    
     if signed_in?
-        redirect_to receipts_url
+      redirect_to receipts_url
     end
+    
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,21 @@
 module ApplicationHelper
 
-    def full_title(title)
-        base_title = "Plannr"
+  def full_title(title)
+    base_title = "Plannr"
 
-        if title.empty?
-            base_title
-        else
-            "#{title} | #{base_title}"
-        end
+    if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
     end
+  end
+
+  def body_id
+    @body_id
+  end
+
+  def body_class
+    @body_class.try(:uniq).try(:join, ' ')
+  end
 
 end
