3320f03c8c9af7bc60c04a7f4e655b7bc7387755
nguyenj <johnnguyen.jln@gmail.com>
2014-05-02 08:39:49 -0500

    initial login modal structure

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -5,6 +5,43 @@ var app = app || {};
 
 app.init = function () {
     app.bgFull(".bg-full");
+    app.bindEvents();
+};
+
+app.modal = {};
+app.modal.settings = {
+    overlay: $('<div />').addClass('modal-overlay'),
+    modalBox: $('<div />').addClass('modal-box'),
+    modalClose: $('<button />').text('X').addClass('modal-close')
+};
+app.modal.open = function ( content ) {
+    var widget = this.settings.overlay.append(this.settings.modalBox.append(this.settings.modalClose, content));
+    $('.page').append(widget);
+    app.modal.bindEvents();
+};
+
+app.modal.close = function () {
+    $('.modal-overlay').remove();
+};
+
+app.modal.bindEvents = function () {
+    $('.modal-close').on('click', app.modal.close);
+};
+
+app.bindEvents = function () {
+    $('.login').on('click', function (e) {
+        var el = $(this),
+            formId = el.data('toggle');
+
+        // $('#'+formId).toggleClass('toggle');
+        if ( $('.modal-overlay').length > 0 ) {
+            app.modal.close();
+        } else {
+            app.modal.open($('#'+formId).toggleClass('toggle'));
+        }
+
+        return false;
+    });
 };
 
 app.bgFull = function ( selector ) {
